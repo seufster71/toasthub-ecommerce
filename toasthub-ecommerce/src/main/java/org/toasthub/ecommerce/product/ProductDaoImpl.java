683885fa3578a869794a8dccee624e9264de74d0
edwardseufert <edwardseufert@localhost>
2021-02-11 19:20:02 -0500

    new

---
+++ toasthub-ecommerce/src/main/java/org/toasthub/ecommerce/product/ProductDaoImpl.java
@@ -0,0 +1,357 @@
+/*
+ * Copyright (C) 2020 The ToastHub Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.toasthub.ecommerce.product;
+
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.toasthub.core.common.EntityManagerDataSvc;
+import org.toasthub.core.common.UtilSvc;
+import org.toasthub.core.general.model.GlobalConstant;
+import org.toasthub.core.general.model.RestRequest;
+import org.toasthub.core.general.model.RestResponse;
+import org.toasthub.core.preference.model.PrefCacheUtil;
+import org.toasthub.ecommerce.model.ECConstant;
+import org.toasthub.ecommerce.model.Product;
+
+@Repository("ECProductDao")
+@Transactional("TransactionManagerData")
+public class ProductDaoImpl implements ProductDao {
+	
+	@Autowired
+	protected EntityManagerDataSvc entityManagerDataSvc;
+	@Autowired
+	protected UtilSvc utilSvc;
+	@Autowired
+	PrefCacheUtil prefCacheUtil;
+	
+	@Override
+	public void delete(RestRequest request, RestResponse response) throws Exception {
+		if (request.containsParam(GlobalConstant.ITEMID) && !"".equals(request.getParam(GlobalConstant.ITEMID))) {
+			
+			Product product = (Product) entityManagerDataSvc.getInstance().getReference(Product.class,  new Long((Integer) request.getParam(GlobalConstant.ITEMID)));
+			entityManagerDataSvc.getInstance().remove(product);
+			
+		} else {
+			utilSvc.addStatus(RestResponse.ERROR, RestResponse.ACTIONFAILED, "Missing ID", response);
+		}
+	}
+
+	@Override
+	public void save(RestRequest request, RestResponse response) throws Exception {
+		Product product = (Product) entityManagerDataSvc.getInstance().getReference(Product.class,  new Long((Integer) request.getParam(ECConstant.PRODUCTID)));
+		entityManagerDataSvc.getInstance().merge(product);
+	}
+
+	@Override
+	public void items(RestRequest request, RestResponse response) throws Exception {
+		String queryStr = "SELECT DISTINCT x FROM Product AS x ";
+		
+		boolean and = false;
+		if (request.containsParam(GlobalConstant.ACTIVE)) {
+			if (!and) { queryStr += " WHERE "; }
+			queryStr += "x.active =:active ";
+			and = true;
+		}
+		
+		if (request.containsParam(ECConstant.PRODUCTID)) {
+			if (!and) { queryStr += " WHERE "; } else { queryStr += " AND "; }
+			queryStr += "x.product.id =:productId ";
+			and = true;
+		} else if (request.containsParam(ECConstant.PROJECTID)) {
+			if (!and) { queryStr += " WHERE "; } else { queryStr += " AND "; }
+			queryStr += "x.project.id =:projectId ";
+			and = true;
+		} else {
+			if (!and) { queryStr += " WHERE "; } else { queryStr += " AND "; }
+			queryStr += "x.product IS NULL AND x.project IS NULL ";
+			and = true;
+		}
+		// search
+		ArrayList<LinkedHashMap<String,String>> searchCriteria = null;
+		if (request.containsParam(GlobalConstant.SEARCHCRITERIA) && !request.getParam(GlobalConstant.SEARCHCRITERIA).equals("")) {
+			if (request.getParam(GlobalConstant.SEARCHCRITERIA) instanceof Map) {
+				searchCriteria = new ArrayList<>();
+				searchCriteria.add((LinkedHashMap<String, String>) request.getParam(GlobalConstant.SEARCHCRITERIA));
+			} else {
+				searchCriteria = (ArrayList<LinkedHashMap<String, String>>) request.getParam(GlobalConstant.SEARCHCRITERIA);
+			}
+			
+			// Loop through all the criteria
+			boolean or = false;
+			
+			String lookupStr = "";
+			for (LinkedHashMap<String,String> item : searchCriteria) {
+				if (item.containsKey(GlobalConstant.SEARCHVALUE) && !"".equals(item.get(GlobalConstant.SEARCHVALUE)) && item.containsKey(GlobalConstant.SEARCHCOLUMN)) {
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_NAME")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "x.name LIKE :nameValue"; 
+						or = true;
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_PRODUCT")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "x.product.name LIKE :productValue"; 
+						or = true;
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_PROJECT")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "x.project.name LIKE :projectValue"; 
+						or = true;
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_STATUS")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "x.active LIKE :statusValue"; 
+						or = true;
+					}
+				}
+			}
+			if (!"".equals(lookupStr)) {
+				if (!and) { 
+					queryStr += " WHERE ( " + lookupStr + " ) ";
+				} else {
+					queryStr += " AND ( " + lookupStr + " ) ";
+				}
+			}
+			
+		}
+		// order by
+		ArrayList<LinkedHashMap<String,String>> orderCriteria = null;
+		StringBuilder orderItems = new StringBuilder();
+		if (request.containsParam(GlobalConstant.ORDERCRITERIA) && !request.getParam(GlobalConstant.ORDERCRITERIA).equals("")) {
+			if (request.getParam(GlobalConstant.ORDERCRITERIA) instanceof Map) {
+				orderCriteria = new ArrayList<>();
+				orderCriteria.add((LinkedHashMap<String, String>) request.getParam(GlobalConstant.ORDERCRITERIA));
+			} else {
+				orderCriteria = (ArrayList<LinkedHashMap<String, String>>) request.getParam(GlobalConstant.ORDERCRITERIA);
+			}
+			
+			// Loop through all the criteria
+			boolean comma = false;
+			
+			for (LinkedHashMap<String,String> item : orderCriteria) {
+				if (item.containsKey(GlobalConstant.ORDERCOLUMN) && item.containsKey(GlobalConstant.ORDERDIR)) {
+					if (item.get(GlobalConstant.ORDERCOLUMN).equals("PM_BACKLOG_TABLE_NAME")){
+						if (comma) { orderItems.append(","); }
+						orderItems.append("x.name ").append(item.get(GlobalConstant.ORDERDIR));
+						comma = true;
+					}
+					if (item.get(GlobalConstant.ORDERCOLUMN).equals("PM_BACKLOG_TABLE_PRODUCT")){
+						if (comma) { orderItems.append(","); }
+						orderItems.append("x.product.name ").append(item.get(GlobalConstant.ORDERDIR));
+						comma = true;
+					}
+					if (item.get(GlobalConstant.ORDERCOLUMN).equals("PM_BACKLOG_TABLE_PROJECT")){
+						if (comma) { orderItems.append(","); }
+						orderItems.append("x.project.name ").append(item.get(GlobalConstant.ORDERDIR));
+						comma = true;
+					}
+					if (item.get(GlobalConstant.ORDERCOLUMN).equals("PM_BACKLOG_TABLE_STATUS")){
+						if (comma) { orderItems.append(","); }
+						orderItems.append("x.active ").append(item.get(GlobalConstant.ORDERDIR));
+						comma = true;
+					}
+				}
+			}
+		}
+		if (!"".equals(orderItems.toString())) {
+			queryStr += " ORDER BY ".concat(orderItems.toString());
+		} else {
+			// default order
+			queryStr += " ORDER BY x.name";
+		}
+		
+		Query query = entityManagerDataSvc.getInstance().createQuery(queryStr);
+		
+		if (request.containsParam(GlobalConstant.ACTIVE)) {
+			query.setParameter("active", (Boolean) request.getParam(GlobalConstant.ACTIVE));
+		}
+		if (request.containsParam(ECConstant.PRODUCTID)) {
+			query.setParameter("productId", new Long((Integer) request.getParam(ECConstant.PRODUCTID)));
+		} else if (request.containsParam(ECConstant.PROJECTID)) {
+			query.setParameter("projectId", new Long((Integer) request.getParam(ECConstant.PROJECTID)));
+		}
+		
+		if (searchCriteria != null){
+			for (LinkedHashMap<String,String> item : searchCriteria) {
+				if (item.containsKey(GlobalConstant.SEARCHVALUE) && !"".equals(item.get(GlobalConstant.SEARCHVALUE)) && item.containsKey(GlobalConstant.SEARCHCOLUMN)) {  
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_NAME")){
+						query.setParameter("nameValue", "%"+((String)item.get(GlobalConstant.SEARCHVALUE)).toLowerCase()+"%");
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_PRODUCT")){
+						query.setParameter("productValue", "%"+((String)item.get(GlobalConstant.SEARCHVALUE)).toLowerCase()+"%");
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_PROJECT")){
+						query.setParameter("projectValue", "%"+((String)item.get(GlobalConstant.SEARCHVALUE)).toLowerCase()+"%");
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_STATUS")){
+						if ("active".equalsIgnoreCase((String)item.get(GlobalConstant.SEARCHVALUE))) {
+							query.setParameter("statusValue", true);
+						} else if ("disabled".equalsIgnoreCase((String)item.get(GlobalConstant.SEARCHVALUE))) {
+							query.setParameter("statusValue", false);
+						}
+					}
+				}
+			}
+		}
+		if (request.containsParam(GlobalConstant.LISTLIMIT) && (Integer) request.getParam(GlobalConstant.LISTLIMIT) != 0){
+			query.setFirstResult((Integer) request.getParam(GlobalConstant.LISTSTART));
+			query.setMaxResults((Integer) request.getParam(GlobalConstant.LISTLIMIT));
+		}
+		@SuppressWarnings("unchecked")
+		List<Product> products = query.getResultList();
+
+		response.addParam(GlobalConstant.ITEMS, products);
+		
+	}
+
+	@Override
+	public void itemCount(RestRequest request, RestResponse response) throws Exception {
+		String queryStr = "SELECT COUNT(DISTINCT x) FROM Product as x ";
+		boolean and = false;
+		if (request.containsParam(GlobalConstant.ACTIVE)) {
+			if (!and) { queryStr += " WHERE "; }
+			queryStr += "x.active =:active ";
+			and = true;
+		}
+		
+		if (request.containsParam(ECConstant.PRODUCTID)) {
+			if (!and) { queryStr += " WHERE "; } else { queryStr += " AND "; }
+			queryStr += "x.product.id =:productId ";
+			and = true;
+		} else if (request.containsParam(ECConstant.PROJECTID)) {
+			if (!and) { queryStr += " WHERE "; } else { queryStr += " AND "; }
+			queryStr += "x.project.id =:projectId ";
+			and = true;
+		} else {
+			if (!and) { queryStr += " WHERE "; } else { queryStr += " AND "; }
+			queryStr += "x.product IS NULL AND x.project IS NULL ";
+			and = true;
+		}
+		
+		ArrayList<LinkedHashMap<String,String>> searchCriteria = null;
+		if (request.containsParam(GlobalConstant.SEARCHCRITERIA) && !request.getParam(GlobalConstant.SEARCHCRITERIA).equals("")) {
+			if (request.getParam(GlobalConstant.SEARCHCRITERIA) instanceof Map) {
+				searchCriteria = new ArrayList<>();
+				searchCriteria.add((LinkedHashMap<String, String>) request.getParam(GlobalConstant.SEARCHCRITERIA));
+			} else {
+				searchCriteria = (ArrayList<LinkedHashMap<String, String>>) request.getParam(GlobalConstant.SEARCHCRITERIA);
+			}
+			
+			// Loop through all the criteria
+			boolean or = false;
+			
+			String lookupStr = "";
+			for (LinkedHashMap<String,String> item : searchCriteria) {
+				if (item.containsKey(GlobalConstant.SEARCHVALUE) && !"".equals(item.get(GlobalConstant.SEARCHVALUE)) && item.containsKey(GlobalConstant.SEARCHCOLUMN)) {
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_NAME")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "x.name LIKE :nameValue"; 
+						or = true;
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_PRODUCT")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "x.product.name LIKE :productValue"; 
+						or = true;
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_PROJECT")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "x.project.name LIKE :projectValue"; 
+						or = true;
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_STATUS")){
+						if (or) { lookupStr += " OR "; }
+						lookupStr += "x.active LIKE :statusValue"; 
+						or = true;
+					}
+				}
+			}
+			if (!"".equals(lookupStr)) {
+				if (!and) { 
+					queryStr += " WHERE ( " + lookupStr + " ) ";
+				} else {
+					queryStr += " AND ( " + lookupStr + " ) ";
+				}
+			}
+			
+		}
+
+		Query query = entityManagerDataSvc.getInstance().createQuery(queryStr);
+		
+		if (request.containsParam(GlobalConstant.ACTIVE)) {
+			query.setParameter("active", (Boolean) request.getParam(GlobalConstant.ACTIVE));
+		}
+		if (request.containsParam(ECConstant.PRODUCTID)) {
+			query.setParameter("productId", new Long((Integer) request.getParam(ECConstant.PRODUCTID)));
+		} else if (request.containsParam(ECConstant.PROJECTID)) {
+			query.setParameter("projectId", new Long((Integer) request.getParam(ECConstant.PROJECTID)));
+		}
+		
+		if (searchCriteria != null){
+			for (LinkedHashMap<String,String> item : searchCriteria) {
+				if (item.containsKey(GlobalConstant.SEARCHVALUE) && !"".equals(item.get(GlobalConstant.SEARCHVALUE)) && item.containsKey(GlobalConstant.SEARCHCOLUMN)) {  
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_NAME")){
+						query.setParameter("nameValue", "%"+((String)item.get(GlobalConstant.SEARCHVALUE)).toLowerCase()+"%");
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_PRODUCT")){
+						query.setParameter("productValue", "%"+((String)item.get(GlobalConstant.SEARCHVALUE)).toLowerCase()+"%");
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_PROJECT")){
+						query.setParameter("projectValue", "%"+((String)item.get(GlobalConstant.SEARCHVALUE)).toLowerCase()+"%");
+					}
+					if (item.get(GlobalConstant.SEARCHCOLUMN).equals("PM_BACKLOG_TABLE_STATUS")){
+						if ("active".equalsIgnoreCase((String)item.get(GlobalConstant.SEARCHVALUE))) {
+							query.setParameter("statusValue", true);
+						} else if ("disabled".equalsIgnoreCase((String)item.get(GlobalConstant.SEARCHVALUE))) {
+							query.setParameter("statusValue", false);
+						}
+					}
+				}
+			}
+		}
+		
+		Long count = (Long) query.getSingleResult();
+		if (count == null){
+			count = 0l;
+		}
+		response.addParam(GlobalConstant.ITEMCOUNT, count);
+		
+	}
+
+	@Override
+	public void item(RestRequest request, RestResponse response) throws Exception {
+		if (request.containsParam(GlobalConstant.ITEMID) && !"".equals(request.getParam(GlobalConstant.ITEMID))) {
+			String queryStr = "SELECT x FROM Product AS x WHERE x.id =:id";
+			Query query = entityManagerDataSvc.getInstance().createQuery(queryStr);
+		
+			query.setParameter("id", new Long((Integer) request.getParam(GlobalConstant.ITEMID)));
+			Product product = (Product) query.getSingleResult();
+			
+			response.addParam(GlobalConstant.ITEM, product);
+		} else {
+			utilSvc.addStatus(RestResponse.ERROR, RestResponse.EXECUTIONFAILED, prefCacheUtil.getPrefText("GLOBAL_SERVICE", "GLOBAL_SERVICE_MISSING_ID",prefCacheUtil.getLang(request)), response);
+		}
+	}
+
+}
